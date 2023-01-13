fce53df6dc69dc04470435eb5d5cabebf30be72f
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2023-01-13 20:05:34 -0300

    initial commit

---
+++ src/CordobaApp.jsx
@@ -0,0 +1,7 @@
+export const CordobaApp = () => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CordobaApp } from './CordobaApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CordobaApp />
+  </React.StrictMode>,
+)
