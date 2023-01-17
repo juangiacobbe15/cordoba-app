7fbbdf9afac71a1db958f051fe6e2f382e509673
juanignaciogiacobbe <jgiacobbe@fi.uba.ar>
2023-01-17 18:37:16 -0300

    added navbar

--- src/CordobaApp.jsx
+++ src/CordobaApp.jsx
@@ -1,9 +1,16 @@
-import { HomePage } from "./pages/HomePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { HomePage, NotFoundPage } from "./pages";
 
 export const CordobaApp = () => {
     return (
         <main className="main-container">
-            <HomePage />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />}></Route>
+                    <Route path="*" element={<NotFoundPage />}></Route>
+                </Routes>
+            </BrowserRouter>
         </main>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
     return (
-        <>
-            <h1>this is my navbar</h1>
-        </>
+        <section className="sticky bg-background flex justify-between text-lg p-3 items-center flex-row">
+            <p className="cursor-pointer font-semibold">Cordoba App</p>
+
+            <ul className="flex flex-row">
+                <Link to='' className="cursor-pointer mr-2">Home</Link>
+                <Link to='' className="cursor-pointer mr-2">Link</Link>
+            </ul>
+        </section>
     );
 }
